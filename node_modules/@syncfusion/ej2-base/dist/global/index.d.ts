60b8e3013b25787e86e7a94d9a13818f84eaf0b1
Mohamed Mokhtar Jaafar <mohamedmokhtar.jaafar@esprit.tn>
2023-05-01 01:12:05 +0100

    Updating Front office

--- node_modules/@syncfusion/ej2-base/dist/global/index.d.ts
+++
@@ -1,5 +0,0 @@
-import * as _base from '@syncfusion/ej2-base';
-
-export declare namespace ej {
-    const base: typeof _base;
-}
--- node_modules/ng-angular-popup/lib/toast.model.d.ts
+++
@@ -1,8 +0,0 @@
-export interface IToast {
-    type?: string;
-    detail?: string;
-    summary?: string;
-    position?: string;
-    duration?: number;
-    sticky?: boolean;
-}
